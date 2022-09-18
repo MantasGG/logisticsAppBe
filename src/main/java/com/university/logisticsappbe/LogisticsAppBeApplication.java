6cb76c62f02faa332204f72e26a7050cf019e2b2
Mantas <mantas.ambrazaitis9@gmail.com>
2022-09-18 23:59:48 +0300

    Initial commit

---
+++ src/main/java/com/university/logisticsappbe/LogisticsAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.university.logisticsappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/university/logisticsappbe/LogisticsAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.logisticsappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
